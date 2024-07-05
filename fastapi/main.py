91732a08ea3738ebb227dd91b82dbafcba9d0232
rofloptichka <adeke.2006@mail.ru>
2024-07-05 12:55:42 +0500

    added download and upload functions from the right folder

--- fastapi/main.py
+++ fastapi/main.py
@@ -5,8 +5,10 @@ from typing import List
 from database import SessionLocal, engine
 import models
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
 import os
 import boto3
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -19,8 +21,7 @@ s3 = boto3.client('s3',
 
 @app.get('/')
 async def check():
-    return os.getenv("S3_BUCKET_NAME")
-
+    return "hehe"
 origins = [
     "http://localhost:3000"
 ]
@@ -110,11 +111,43 @@ async def upload(file: UploadFile = File(...)):
             if not bucket_name:
                 raise ValueError("Bucket name is not set")
 
-            print(file.filename)
-            s3.upload_fileobj(file.file, bucket_name, file.filename)
-            return {"message": "File uploaded successfully"}
+            folder_name = os.getenv("FOLDER")
+            object_name = f"{folder_name}/{file.filename}"
+
+            print(f"Uploading {file.filename} to {folder_name}")
+            s3.upload_fileobj(file.file, bucket_name, object_name)
+            return {"message": f"File uploaded successfully to {folder_name}"}
         else:
             return {"error": "No file provided"}
     except Exception as e:
         print(f"Error occurred: {e}")
         raise HTTPException(status_code=500, detail=f"Error occurred: {e}")
+
+
+
+@app.get("/download")
+async def download_file(file_name: str):
+    try:
+        bucket_name = os.getenv("S3_BUCKET_NAME")
+        folder_name = os.getenv("FOLDER")
+        object_name = f"{folder_name}/{file_name}"
+
+        file_obj = s3.get_object(Bucket=bucket_name, Key=object_name)
+        file_stream = file_obj['Body']
+        
+        return StreamingResponse(file_stream, media_type='application/octet-stream', headers={"Content-Disposition": f"attachment; filename={file_name}"})
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        raise HTTPException(status_code=500, detail=f"Error occurred: {e}")
+    
+
+
+@app.get("/getunsolvedreports")
+async def get_unsolved():
+    try:
+        res = s3.list_objects_v2(Bucket=os.getenv("S3_BUCKET_NAME"))
+        return res
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        raise HTTPException(status_code=500, detail=f"Error occurred: {e}")
+    
